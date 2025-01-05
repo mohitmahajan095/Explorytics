f638e7dd31e87a12b030eb0f648ac55cec335773
Mohit Mahajan <73893201+mohitmahajan095@users.noreply.github.com>
2025-01-05 19:29:25 +0530

    Update __init__.py

--- src/explorytics/__init__.py
+++ src/explorytics/__init__.py
@@ -1,4 +1,11 @@
 # src/explorytics/__init__.py
+
+# Explorytics: A Python library for effortless exploratory data analysis.
+# Copyright (c) 2025 Mohit Mahajan
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 from .core.analyzer import DataAnalyzer
 from .visualizations.plots import DataVisualizer
 
